d920a7afc90724df75afb814176ec89c102da766
Vaibhav MS <mycodingv41@gmail.com>
2026-03-01 15:44:20 +0530

    feat:Add loder for progress during submission

--- apps/web/components/Loder.tsx
+++ apps/web/components/Loder.tsx
@@ -10,3 +10,14 @@ export const Loder = ({title}: {title:string}) => {
                       </div>
     )
 } 
+export const SubmissionLoder = ({progress}: {progress:number}) => {
+    return (
+        <div className={`absolute bottom-0 left-0 h-[2px] transition-all duration-500 ease-in-out
+    ${progress === 100 
+      ? " bg-white shadow-[0_0_15px_rgba(134,239,172,1)] scale-y-150" 
+      : "bg-green-300 animate-pulse"
+    }`}
+        style={{width:`${progress}%`,
+        transitionProperty: 'width, box-shadow, background-color'}}></div>
+    )
+} 
